8639b21fc2df203b9bda97ab35547c7bed728b0c
Juan Salazar <ferthelet@gmail.com>
2023-04-26 09:53:45 -0500

    checks the 5th book

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ function fetchBooks() {
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  let counter = 0;
+  let fifthBook = '';
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    counter++;
+
+    if (counter === 5) {
+      h2.innerHTML = '5th book ==> ';
+    } 
+    h2.innerHTML += book.name;
     main.appendChild(h2);
   });
 }
