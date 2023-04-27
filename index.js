d81de76bab1fed72a4b9139dc153bead3ac8c514
Juan Salazar <ferthelet@gmail.com>
2023-04-27 14:22:43 -0500

    fixes forEach not a function error

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function fetchBooks() {
 function renderBooks(books) {
   const main = document.querySelector('main');
   let counter = 0;
-  let fifthBook = '';
+  const fifthBook = 5;
 
   books.forEach(book => {
     const h2 = document.createElement('h2');
     counter++;
 
-    if (counter === 5) {
+    if (counter === fifthBook) {
       h2.innerHTML = '5th book ==> ';
     } 
     h2.innerHTML += book.name;
@@ -24,5 +24,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  renderBooks(fetchBooks());
+  // was getting the forEach error since calling it wrongly
+  fetchBooks();
 });
